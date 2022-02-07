9b30d05d1678533473d122f69aa7c4da8ee88d02
saidulislam <saidul.islam@gmail.com>
2022-02-07 12:13:40 -0500

    updated docker compose and added logging

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,8 @@ MAIL_SERVER = 'smtp.mailtrap.io'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
-MAIL_USERNAME = 'asdfasdfasdfasdf'
-MAIL_PASSWORD = 'asdfasdfasdfasdf'
+MAIL_USERNAME = 'asdf'
+MAIL_PASSWORD = 'asdf'
 
 # Celery.
 CELERY_BROKER_URL = 'redis://:devpassword@redis:6379/0'
